8f86fe67ab01dcc244058fe9ca469755032ac17c
jingyang jiang <jingyang.jiang@mail.mcgill.ca>
2021-06-21 14:48:40 -0400

    code added

---
+++ cmake-build-debug/CMakeFiles/Comp310_A3.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cpu.c" "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cmake-build-debug/CMakeFiles/Comp310_A3.dir/cpu.c.o"
+  "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/interpreter.c" "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cmake-build-debug/CMakeFiles/Comp310_A3.dir/interpreter.c.o"
+  "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/kernel.c" "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cmake-build-debug/CMakeFiles/Comp310_A3.dir/kernel.c.o"
+  "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/memorymanager.c" "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cmake-build-debug/CMakeFiles/Comp310_A3.dir/memorymanager.c.o"
+  "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/pcb.c" "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cmake-build-debug/CMakeFiles/Comp310_A3.dir/pcb.c.o"
+  "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/ram.c" "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cmake-build-debug/CMakeFiles/Comp310_A3.dir/ram.c.o"
+  "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/shell.c" "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cmake-build-debug/CMakeFiles/Comp310_A3.dir/shell.c.o"
+  "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/shellmemory.c" "/cygdrive/c/Users/erres/CLionProjects/Comp310 A3/cmake-build-debug/CMakeFiles/Comp310_A3.dir/shellmemory.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Comp310_A3.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Comp310_A3.dir/cpu.c.o"
+  "CMakeFiles/Comp310_A3.dir/interpreter.c.o"
+  "CMakeFiles/Comp310_A3.dir/kernel.c.o"
+  "CMakeFiles/Comp310_A3.dir/memorymanager.c.o"
+  "CMakeFiles/Comp310_A3.dir/pcb.c.o"
+  "CMakeFiles/Comp310_A3.dir/ram.c.o"
+  "CMakeFiles/Comp310_A3.dir/shell.c.o"
+  "CMakeFiles/Comp310_A3.dir/shellmemory.c.o"
+  "Comp310_A3.exe"
+  "Comp310_A3.pdb"
+  "libComp310_A3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Comp310_A3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
